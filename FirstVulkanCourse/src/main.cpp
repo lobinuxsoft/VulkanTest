ab118a9492e0bd777ec134415c06e5ab542653ca
Matias Galarza <lobinuxsoft@gmail.com>
2025-01-02 18:02:22 -0300

    Complete section 17

--- FirstVulkanCourse/src/main.cpp
+++ FirstVulkanCourse/src/main.cpp
@@ -1,7 +1,28 @@
+#include <cmath>
+#include <cstdint>
+#include <gsl/gsl>
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!!" << std::endl;
-    return 0;
+class Point {
+  std::float_t x_, y_, z_;
+
+  void MyMethod() const {}
+};
+
+void MyFunction() {}
+
+int main(std::int32_t argc, gsl::zstring* argv) {
+  gsl::span<gsl::zstring> arguments(argv, argc);
+  std::sort(arguments.begin(), arguments.end());
+
+  std::string my_string_variable;
+
+  gsl::not_null<gsl::zstring*> arguments_pointer_validated = argv;
+
+  std::uint32_t count = 0;
+  std::size_t size = 12;
+  std::float_t s = 0.0f;
+
+  std::cout << "Hello World!!" << std::endl;
+  return 0;
 }
